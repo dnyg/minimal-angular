5497476606eb68d2b4bc4c6681537f3084853f92
Daniel Nygaard <daniel@codings.dk>
2018-11-02 20:07:03 +0100

    change to minimal angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,8 @@
-import * as $ from 'jquery';
-import { AfterViewInit, Component, ElementRef, NgZone, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<div #container></div>'
+  template: '<div></div>'
 })
-export class AppComponent implements AfterViewInit {
-  @ViewChild('container') element: ElementRef;
-
-  ngAfterViewInit() {
-    setTimeout(() => {
-      ($(this.element.nativeElement) as any).fullCalendar();
-    });
-  }
+export class AppComponent {
 }
